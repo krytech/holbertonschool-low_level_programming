e8fa371c4ebc4b41d1c39f394e2f9a31f502c955
Carson Stearn <cstearn@gmail.com>
2021-02-04 23:54:52 +0000

    last digit 2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,17 +13,18 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-prinf("Last digit of %i is %i and is", n, (n % 10));
+printf("Last digit of %i is %i and is ", n, (n % 10));
 if ((n % 10) > 5)
 {
-printf("and is greater than 5\n");
+printf("greater than 5\n");
 }
 else if ((n % 10) == 0)
 {
-printf("and is 0\n");
+printf("0\n");
+}
 else
 {
-printf("and is less than 6 and not 0");
+printf("less than 6 and not 0\n");
 }
 return (0);
 }
