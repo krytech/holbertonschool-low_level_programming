911361e8332f2a5fd0f11768bbd09938aa7d3fcd
Carson Stearn <cstearn@gmail.com>
2021-02-05 03:53:24 +0000

    numbarz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - print nunbers using putchar
+ *Return: 0
+ */
+
+int main(void)
+{
+int n = '0';
+while (n <= '9')
+{
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
+}
