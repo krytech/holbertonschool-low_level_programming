5dbea73af6f1be2b2300bd11fc86e139eda6bbae
Carson Stearn <cstearn@gmail.com>
2021-02-09 19:31:07 +0000

    number check stuff

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_sign - prints and checks the number value
+ * @n: number value to check
+ * Return: 1 if greater than zero, 0 if zero, -1 if negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
