deafbc3cb4e6cb03083f9762a4ebc6efba09107b
Carson Stearn <cstearn@gmail.com>
2021-02-09 19:42:08 +0000

    absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _abs - returns absolute number value
+ * @n: number value
+ * Return: absolute numerical value
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
