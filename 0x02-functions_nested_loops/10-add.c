1d2092b3c998feed75c51520bf4162e01c7fe041
Carson Stearn <cstearn@gmail.com>
2021-02-10 02:49:03 +0000

    add2

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 
 /**
- * add - add two numbers
+ * add - add two ints
  * @a: first int
  * @b: second int
  * Return: sum of both ints
  */
 
-int add (int a, int b)
+int add(int a, int b)
 {
 	return (a + b);
 }
