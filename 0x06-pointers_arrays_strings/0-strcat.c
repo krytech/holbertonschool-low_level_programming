38f22cd74ea4a947d1845f83aa43cebb7e0e27df
Carson Stearn <cstearn@gmail.com>
2021-02-18 17:18:42 +0000

    q1 and other files

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * *_strcat - copy one string and add it to the other
+ * @src: first string
+ * @dest: second string
+ * Return: full string with added value
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int x = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[x] != '\0')
+	{
+		dest[i] = src[x];
+		i++;
+		x++;
+	}
+	return (dest);
+}
