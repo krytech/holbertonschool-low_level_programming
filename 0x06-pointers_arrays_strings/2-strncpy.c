a3614ae388cb5d30e8c8c449f07e5c002224a575
Carson Stearn <cstearn@gmail.com>
2021-02-18 23:28:38 +0000

    string copy n

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strncat - copies a string using n value
+ * @src: string to use
+ * @dest: string to copy to
+ * @n: characters of string to copy
+ * Return: dest final string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
