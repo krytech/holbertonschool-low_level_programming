cdeb33f778037782ab1842a602fb11e52d03af09
Carson Stearn <cstearn@gmail.com>
2021-02-19 05:12:44 +0000

    capitalize stand alone words

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * cap_string - capitalize the first letter of every word
+ * @a: letter string
+ * Return: string
+ */
+
+char *cap_string(char *a)
+{
+	int i = 0;
+	int x = 0;
+	int smbl[14] = {' ', '\t', '\n', ',', ';', '.', '!','?', '"', '(', ')',
+			'{', '}'};
+
+	while (a[i] != '\0')
+	{
+		if (a[i] >= 'a' && a[i] <= 'z')
+		{
+			for (x = 0; x < 14; x++)
+			{
+				if (a[i - 1] == smbl[x])
+				{
+					a[i] = a[i] - 32;
+				}
+			}
+		}
+		i++;
+	}
+	return (a);
+}
