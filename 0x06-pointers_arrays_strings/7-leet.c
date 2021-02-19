95a22d259bdde3ffe71c55c40b9b8d8c4eae22e9
Carson Stearn <cstearn@gmail.com>
2021-02-19 18:39:25 +0000

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * leet - replace letters with numbers to make it leet
+ * @c: character
+ * Return: full string with changes
+ */
+
+char *leet(char *a)
+{
+	int i;
+	int x;
+	char check[] = "aAeEoOtTlL";
+	char change[] = "4433007711";
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		for (x = 0; check[x]; x++)
+			if (check[x] == a[i])
+			{
+				a[i] = change[x];
+				break;
+			}
+	}
+	return (a);
+}
