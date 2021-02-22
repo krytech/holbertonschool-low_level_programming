d55f4db6aad265604cec0a2d6fb6a78cb45ac23d
Carson Stearn <cstearn@gmail.com>
2021-02-22 23:43:28 +0000

    task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * _strstr - finds a substring in a string
+ * @haystack: stirng to check
+ * @needle: chars to find
+ * Return: matched string
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int x;
+	int y;
+
+	while (haystack[i] != '\0')
+	{
+		if (haystack[i] == needle[0])
+		{
+			x = i, y = 0;
+			while (needle[y] != '\0')
+			{
+				if (haystack[x] == needle[y])
+					x++, y++;
+				else
+					break;
+			}
+			if (needle[y] == '\0')
+			{
+				return (haystack + i);
+			}
+		}
+		i++;
+	}
+	return (0);
+}
