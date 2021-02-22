2d9125f44627f986c7939aa845c0af0206e4c455
Carson Stearn <cstearn@gmail.com>
2021-02-22 18:05:20 +0000

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * memcpy - copies n bytes from src to dest
+ * @n: bytes to copy
+ * @src: copy from
+ * @dest: copy to
+ * Return: dest string
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	for (; n > 0; n--)
+	{
+		dest[n - 1] = src[n - 1];
+	}
+	return (dest);
+}
