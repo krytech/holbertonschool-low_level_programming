15cfff31c8556bc9d900939ffe5e5195791afd81
Carson Stearn <cstearn@gmail.com>
2021-02-23 00:28:42 +0000

    task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_chessboard - prints a chessboard with letters
+ * @a: chessboard array
+ * Return: 0
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int row;
+	int column;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (column = 0; column < 8; column++)
+		{
+			_putchar(a[row][column]);
+		}
+		_putchar('\n');
+	}
+}
