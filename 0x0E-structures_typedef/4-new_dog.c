1dea16fbb0e01d318131df4a190a825ddbab5d26
Carson Stearn <cstearn@gmail.com>
2021-03-09 03:20:06 +0000

    task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,54 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog entry in the dog struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: pointer to new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newdog;
+	int n_len = 0; /* name length */
+	int o_len = 0; /* owner length */
+	int i;
+	char *n_ph; /* name placeholder for copying */
+	char *o_ph; /* owner placeholder for copying */
+
+	newdog = malloc(sizeof(dog_t));
+	if (!newdog)
+		return (NULL);
+
+	while (name[n_len])
+		n_len++;
+	while (owner[o_len])
+		o_len++;
+
+	n_ph = malloc(n_len + 1);
+	if (!n_ph)
+	{
+		free(newdog);
+		return (NULL);
+	}
+	o_ph = malloc(o_len + 1);
+	if (!o_ph)
+	{
+		free(n_ph);
+		free(newdog);
+		return (NULL);
+	}
+
+	for (i = 0; i < n_len; i++)
+		n_ph[i] = name[i];
+	for (i = 0; i < o_len; i++)
+		o_ph[i] = owner[i];
+
+	newdog->name = n_ph;
+	newdog->age = age;
+	newdog->owner = o_ph;
+
+	return (newdog);
+}
