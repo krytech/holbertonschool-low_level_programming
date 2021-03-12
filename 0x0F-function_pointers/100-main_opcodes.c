470deec46b8840af2b4da2481806c2292741c257
Carson Stearn <cstearn@gmail.com>
2021-03-12 18:43:43 +0000

    task 4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int num;
+	int i;
+
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	num = atoi(argv[1]);
+	if (num < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (i = 0; i < (num - 1); i++)
+	{
+		printf("%02x ", ((unsigned char *)main)[i]);
+	}
+	printf("%02x", ((unsigned char *)main)[i]);
+	printf("\n");
+
+	return (0);
+}
