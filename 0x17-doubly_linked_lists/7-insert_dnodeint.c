45559f21aa23cf56498cd423b6f731f77a0f4309
Carson Stearn <cstearn@gmail.com>
2021-05-03 00:31:14 +0000

    betty fix spacing

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,29 +10,29 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *new_node, *ph = *h;
-    unsigned int i;
+	dlistint_t *new_node, *ph = *h;
+	unsigned int i;
 
-    if (idx == 0)
-        return (add_dnodeint(h, n));
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
-    new_node = malloc(sizeof(dlistint_t));
-        if (!new_node)
-            return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+		if (!new_node)
+			return (NULL);
 
-    new_node->n = n;
+	new_node->n = n;
 
-    for (i = 1; ph; i++, ph = ph->next)
-        if (i == idx)
-        {
-            new_node->next = ph->next;
-            if (new_node->next)
-                new_node->next->prev = new_node;
-            new_node->prev = ph;
-            ph->next = new_node;
+	for (i = 1; ph; i++, ph = ph->next)
+		if (i == idx)
+		{
+			new_node->next = ph->next;
+			if (new_node->next)
+				new_node->next->prev = new_node;
+			new_node->prev = ph;
+			ph->next = new_node;
 
-            return (new_node);
-        }
-    free(new_node);
-    return (NULL);
+			return (new_node);
+		}
+	free(new_node);
+	return (NULL);
 }
