eb7ebfa52dc5beae7f87774de22f93f6f1ca6287
Carson Stearn <cstearn@gmail.com>
2021-05-28 19:58:50 +0000

    task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,40 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table to delete
+ * Return: void
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int index = 0;
+	hash_node_t *node, *next;
+
+	if (!ht)
+		return;
+
+	if (!(ht->array))
+	{
+		free(ht);
+		return;
+	}
+
+	while (index < ht->size)
+	{
+		node = (ht->array)[index];
+		while (node)
+		{
+			next = node->next;
+			if (node->key)
+				free(node->key);
+			if (node->value)
+				free(node->value);
+			free(node);
+			node = next;
+		}
+		index++;
+	}
+	free(ht->array);
+	free(ht);
+}
