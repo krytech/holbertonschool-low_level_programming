49176b4dccbe6dc60de555faf2e18311f79c3a3c
Carson Stearn <cstearn@gmail.com>
2021-07-02 16:24:27 +0000

    init

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Function that returns the perimeter of an island."""
+
+
+def island_perimeter(grid):
+    """Calculates the perimeter when '1' is found"""
+    beaches = 0
+    for row in range(len(grid)):
+        for col in range (len(grid[0])):
+            if grid[row][col] == 1:
+                if grid[row - 1][col] == 0:
+                    beaches += 1 # north beach
+                if grid[row + 1][col] == 0:
+                    beaches += 1 # south beach
+                if grid[row][col - 1] == 0:
+                    beaches += 1 # west beach
+                if grid[row][col + 1] == 0:
+                    beaches += 1 # east beach
+    return beaches
