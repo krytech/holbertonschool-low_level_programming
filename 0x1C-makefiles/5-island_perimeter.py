f75714bc41efa4cd65cb50833c0f1813a9a3305f
Carson Stearn <cstearn@gmail.com>
2021-07-02 16:38:11 +0000

    fix going out of range

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,12 +8,12 @@ def island_perimeter(grid):
     for row in range(len(grid)):
         for col in range (len(grid[0])):
             if grid[row][col] == 1:
-                if grid[row - 1][col] == 0:
+                if grid[row - 1][col] == 0 or row == 0:
                     beaches += 1 # north beach
-                if grid[row + 1][col] == 0:
+                if grid[row + 1][col] == 0 or row == (len(grid) - 1):
                     beaches += 1 # south beach
-                if grid[row][col - 1] == 0:
+                if grid[row][col - 1] == 0 or col == 0:
                     beaches += 1 # west beach
-                if grid[row][col + 1] == 0:
+                if grid[row][col + 1] == 0 or col == (len(grid[0]) + 1):
                     beaches += 1 # east beach
     return beaches
