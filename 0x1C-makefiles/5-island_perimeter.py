8cc13c8c1b0bb1c582449ade4e7159736caecdf6
Carson Stearn <cstearn@gmail.com>
2021-07-02 16:47:15 +0000

    pep8 and silly mistakes

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,12 +8,12 @@ def island_perimeter(grid):
     for row in range(len(grid)):
         for col in range(len(grid[0])):
             if grid[row][col] == 1:
-                if row == 0 or grid[row - 1][col]:
+                if row == 0 or grid[row - 1][col] == 0:
                     beaches += 1  # north beach
                 if row == (len(grid) - 1) or grid[row + 1][col] == 0:
                     beaches += 1  # south beach
                 if col == 0 or grid[row][col - 1] == 0:
                     beaches += 1  # west beach
-                if col == (len(grid[0]) + 1) or grid[row][col + 1] == 0:
+                if col == (len(grid[0]) - 1) or grid[row][col + 1] == 0:
                     beaches += 1  # east beach
     return beaches
