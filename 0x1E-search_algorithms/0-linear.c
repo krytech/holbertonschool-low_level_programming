5bfcfb767479e7133e32c1512d14c5488bc912af
Carson Stearn <cstearn@gmail.com>
2021-08-31 17:39:34 +0000

    fixing t0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,15 +12,14 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (array)
-	{
-		for (i = 0; i < size; i++)
-		{
-			printf("Value checked array [%lu] = [%d]\n", i, array[i]);
-			if (array[i] == value)
-				return (i);
-		}
+	if (!array)
 		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array [%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
 	}
 	return (-1);
 }
